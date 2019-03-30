e11bf602b3f0bc732bb72b9f930a3cc6fca3b362
theresa-li <tli2@cca.edu>
2019-03-29 18:04:47 -0700

    Refactor database seeder

---
+++ database/index.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const db = mongoose.connect('mongodb://localhost/reviewsAndRatings', { useNewUrlParser: true });
+
+
+
+module.exports = db;
--- database/seed.js
+++ database/seed.js
@@ -1,36 +1,12 @@
-const mongoose = require('mongoose');
-const db = mongoose.connect('mongodb://localhost/reviewsAndRatings', { useNewUrlParser: true });
+const db = require('./index.js');
+const Review = require('./Review.js');
 
 const faker = require('faker');
-
-const reviewSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  user: {
-    username: String,
-    nationality: String,
-    groupType: String,
-    ageRange: String,
-    totalReviews: Number
-  },
-  ratings: {
-    valueForMoney: Number,
-    location: Number,
-    atmosphere: Number,
-    facilities: Number,
-    security: Number,
-    staff: Number,
-    cleanliness: Number
-  },
-  language: String,
-  review: String,
-  date: Date
-});
-
-const Review = mongoose.model('Review', reviewSchema);
 
 let sampleReviews = [];
 const groupType = ["Female", "Male", "Mixed Group"];
 const ageRange = ["18-21", "18-21", "18-21", "18-21", "18-21", "21-35", "21-35", "21-35", "35-50", "50+"];
+const nationality = ["United States", "United States", "United States", "United Kingdom", "United Kingdom", "France", "Germany", "Brazil"];
 
 for (let i = 0; i < 3; i++) {
   var randomNum = Math.random() * Math.floor(10);
@@ -38,9 +14,9 @@ for (let i = 0; i < 3; i++) {
   sampleReviews.push({
     user: {
       username: faker.internet.userName(),
-      nationality: faker.address.country(),
+      nationality: nationality[Math.floor(Math.random() * Math.floor(nationality.length))],
       groupType: groupType[Math.floor(Math.random() * Math.floor(groupType.length))],
-      ageRange: ageRange[Math.floor(Math.random() * Math.floor(groupType.length))],
+      ageRange: ageRange[Math.floor(Math.random() * Math.floor(ageRange.length))],
       totalReviews: faker.random.number(40)
     },
     ratings: {
@@ -63,6 +39,6 @@ const insertReviews = () => {
     .then(() => db.disconnect());
 }
 
-// insertReviews();
+insertReviews();
 
 console.log(sampleReviews);

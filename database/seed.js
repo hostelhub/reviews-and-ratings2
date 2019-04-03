7b467add115917610a1462a0e38c3b97e4a0785f
theresa-li <tli2@cca.edu>
2019-04-03 14:56:45 -0700

    Refactor schema and database seeder

--- database/seed.js
+++ database/seed.js
@@ -3,39 +3,80 @@ const Review = require('./Review.js');
 
 const faker = require('faker');
 
-let sampleReviews = [];
+const hostels = [];
 const groupType = ["Female", "Male", "Mixed Group"];
 const ageRange = ["18-21", "18-21", "18-21", "18-21", "18-21", "21-35", "21-35", "21-35", "35-50", "50+"];
 const nationality = ["United States", "United States", "United States", "United Kingdom", "United Kingdom", "France", "Germany", "Brazil"];
 
-for (let i = 0; i < 100; i++) {
-  var randomNum = Math.random() * Math.floor(10);
-  
-  sampleReviews.push({
-    user: {
-      username: faker.internet.userName(),
-      nationality: nationality[Math.floor(Math.random() * Math.floor(nationality.length))],
-      groupType: groupType[Math.floor(Math.random() * Math.floor(groupType.length))],
-      ageRange: ageRange[Math.floor(Math.random() * Math.floor(ageRange.length))],
-      totalReviews: faker.random.number(40)
-    },
-    ratings: {
-      valueForMoney: faker.random.number({min:randomNum, max:10}),
-      location: faker.random.number({min:randomNum, max:10}),
-      atmosphere: faker.random.number({min:randomNum, max:10}),
-      facilities: faker.random.number({min:randomNum, max:10}),
-      security: faker.random.number({min:randomNum, max:10}),
-      staff: faker.random.number({min:randomNum, max:10}),
-      cleanliness: faker.random.number({min:randomNum, max:10}),
-    },
-    language: faker.random.boolean(chance_of_getting_true=90),
-    review: faker.lorem.paragraph(),
-    date: faker.date.past()
+for (let h = 1; h < 101; h += 1) {
+  let reviews = [];
+  const randomAmtOfReviews = faker.random.number({min:40, max:1000});
+
+  for (let r = 0; r < randomAmtOfReviews; r += 1) {
+    const randomNum = faker.random.number(10);
+
+    reviews.push({
+      user: {
+        username: faker.internet.userName(),
+        nationality: nationality[Math.floor(Math.random() * Math.floor(nationality.length))],
+        groupType: groupType[Math.floor(Math.random() * Math.floor(groupType.length))],
+        ageRange: ageRange[Math.floor(Math.random() * Math.floor(ageRange.length))],
+        totalReviews: faker.random.number(40)
+      },
+      ratings: {
+        valueForMoney: faker.random.number({min:randomNum, max:10}),
+        location: faker.random.number({min:randomNum, max:10}),
+        atmosphere: faker.random.number({min:randomNum, max:10}),
+        facilities: faker.random.number({min:randomNum, max:10}),
+        security: faker.random.number({min:randomNum, max:10}),
+        staff: faker.random.number({min:randomNum, max:10}),
+        cleanliness: faker.random.number({min:randomNum, max:10}),
+      },
+      language: faker.random.boolean(chance_of_getting_true=90),
+      review: faker.lorem.paragraph(),
+      date: faker.date.past()
+    })
+  }
+
+  hostels.push({
+    _id: h,
+    reviews: reviews
   });
 }
 
+// for (let i = 1; i < 10; i += 1) {
+//   const randomAmt = faker.random.number({min:40, max:1000});
+
+//   // for (let r = 0; r < randomAmt; r += 1) {
+//       const randomNum = faker.random.number(10);
+      
+//       sampleReviews.push({
+//         _id: i,
+//         user: {
+//           username: faker.internet.userName(),
+//           nationality: nationality[Math.floor(Math.random() * Math.floor(nationality.length))],
+//           groupType: groupType[Math.floor(Math.random() * Math.floor(groupType.length))],
+//           ageRange: ageRange[Math.floor(Math.random() * Math.floor(ageRange.length))],
+//           totalReviews: faker.random.number(40)
+//         },
+//         ratings: {
+//           valueForMoney: faker.random.number({min:randomNum, max:10}),
+//           location: faker.random.number({min:randomNum, max:10}),
+//           atmosphere: faker.random.number({min:randomNum, max:10}),
+//           facilities: faker.random.number({min:randomNum, max:10}),
+//           security: faker.random.number({min:randomNum, max:10}),
+//           staff: faker.random.number({min:randomNum, max:10}),
+//           cleanliness: faker.random.number({min:randomNum, max:10}),
+//         },
+//         language: faker.random.boolean(chance_of_getting_true=90),
+//         review: faker.lorem.paragraph(),
+//         date: faker.date.past()
+//       });
+//   // }
+// }
+
 const insertReviews = () => {
-  Review.create(sampleReviews)
+  Review.create(hostels)
     .then(() => db.disconnect());
 }
 

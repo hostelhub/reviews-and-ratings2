5633e0c7d7a052d8a54ab02810204f01e005ceb2
theresa-li <tli2@cca.edu>
2019-04-04 16:23:54 -0700

    Implement TotalRatings component

--- client/src/components/ReviewsAndRatings.jsx
+++ client/src/components/ReviewsAndRatings.jsx
@@ -1,16 +1,71 @@
 import React from 'react';
+import TotalRatings from './TotalRatings.jsx'
 
 class ReviewsAndRatings extends React.Component {
   constructor (props) {
     super(props)
   }
 
+  findAverage (reviews) {
+    let ratings = {
+      atmosphere: [],
+      cleanliness: [],
+      facilities: [],
+      location: [],
+      security: [],
+      staff: [],
+      valueForMoney: []
+    };
+
+    for (let r = 0; r < reviews.length; r += 1) {
+      const ratingEntries = Object.entries(reviews[r].ratings);
+      for (let e = 0; e < ratingEntries.length; e += 1) {
+        ratings[ratingEntries[e][0]].push(ratingEntries[e][1]);
+      }
+    }
+
+    const allRatingEntries = Object.entries(ratings);
+    for (let a = 0; a < allRatingEntries.length; a += 1) {
+     ratings[allRatingEntries[a][0]] = this.findAverageRating(allRatingEntries[a][1]);
+    }
+
+    return ratings;
+  }
+
+  findAverageRating (ratings) {
+    const total = ratings.reduce((acc, currVal) => acc + currVal);
+    return Math.round((total / ratings.length) * 10) / 10;
+  }
+
   render () {
+    const ratings = this.findAverage(this.props.reviews.reviews);
+    const totalRatings = this.findAverageRating(Object.values(ratings));
     return (
       <div id="reviewsAndRatings">
-        <div class="title">Reviews & Ratings</div>
+        <h2>Reviews & Ratings</h2>
+        <TotalRatings 
+          totalRatings={totalRatings}
+          amtOfRatings={this.props.reviews.reviews.length}/>
       </div>
     )
+  }
+}
+
+ReviewsAndRatings.defaultProps = {
+  reviews: {
+    reviews:[
+      {
+        ratings:{
+          atmosphere: [],
+          cleanliness: [],
+          facilities: [],
+          location: [],
+          security: [],
+          staff: [],
+          valueForMoney: []
+        }
+      }
+    ]
   }
 }
 
---
+++ client/src/components/TotalRatings.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class TotalRatings extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+  
+  render () {
+    return(
+      <div id="totalRatings">
+      </div>
+    )
+  }
+}
+
+export default TotalRatings;

2d49024f90da925ded63dfbbed3ac6fb46f002b6
theresa-li <tli2@cca.edu>
2019-04-06 12:10:07 -0700

    Implement getScoreRank for getting hostel ranks.

--- client/src/components/TotalRatings.jsx
+++ client/src/components/TotalRatings.jsx
@@ -4,10 +4,29 @@ class TotalRatings extends React.Component {
   constructor (props) {
     super(props)
   }
+
+  getScoreRank (score) {
+    if (score >= 9) {
+      return "Superb";
+    } else if (score >= 8) {
+      return "Fabulous";
+    } else if (score >= 7) {
+      return "Very Good";
+    } else if (score >= 6) {
+      return "Good";
+    } else {
+      return "Rating";
+    }
+  }
   
   render () {
     return(
-      <div id="totalRatings">
+      <div className="totalRatings">
+        <div className="score">{this.props.totalRatings}</div>
+        <div className="ratingDetails">
+          <div className="scoreRank">{this.getScoreRank(Number(this.props.totalRatings))}</div>
+          <div className="totalReviews">Based on {this.props.amtOfRatings} reviews</div>
+        </div>
       </div>
     )
   }

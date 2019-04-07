2478a2a622d914d534b1bc863d74e36e03dfbd91
theresa-li <tli2@cca.edu>
2019-04-07 16:46:19 -0700

    Implement code for IndividualRatingsEntries

--- client/src/components/IndividualRatingsEntries.jsx
+++ client/src/components/IndividualRatingsEntries.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class IndividualRatingsEntries extends React.PureComponent {
+  static capitalizeFirstChar(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
   render() {
     const { ratingType } = this.props;
     const { rating } = this.props;
     return (
       <div>
-        <p>{JSON.stringify(ratingType)}</p>
-        <p>{JSON.stringify(rating)}</p>
+        <div>
+          <div>{IndividualRatingsEntries.capitalizeFirstChar(ratingType)}</div>
+          <div>{rating}</div>
+        </div>
+        <div>
+          <div></div>
+        </div>
       </div>
     );
   }

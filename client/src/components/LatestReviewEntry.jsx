b4ad67184b4d8c5177418c003a00d4d168209d24
Theresa <2litheresa@gmail.com>
2019-10-16 19:07:00 -0700

    Edit review styling

--- client/src/components/LatestReviewEntry.jsx
+++ client/src/components/LatestReviewEntry.jsx
@@ -11,6 +11,9 @@ ReviewEntry.displayName = 'ReviewEntry';
 
 const ScoreAndDate = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 10px;
 `;
 
 const ScoreDetails = styled.div`
